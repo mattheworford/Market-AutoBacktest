5763105ed108bb6d25eccf2d321bcb7293a89463
Matthew Orford <mattorford9@gmail.com>
2024-08-04 15:00:34 -0400

    refactor to use env

--- data_ingestion/fetch_quandl.py
+++ data_ingestion/fetch_quandl.py
@@ -1,13 +1,15 @@
 import requests
 import pandas as pd
 from io import StringIO
-
-from config.config import QUANDL_API_KEY
+import os
+from dotenv import load_dotenv
 
 
 def fetch_quandl_data(
     symbol: str, start_date: str = "2020-01-01", end_date: str = "2024-01-01"
 ) -> pd.DataFrame:
+    load_dotenv()
+    QUANDL_API_KEY = os.getenv("QUANDL_API_KEY")
     url = f"https://www.quandl.com/api/v3/datasets/WIKI/{symbol}.csv"
     params = {"start_date": start_date, "end_date": end_date, "api_key": QUANDL_API_KEY}
     response = requests.get(url, params=params)
--- tests/test_fetch_quandl.py
+++ tests/test_fetch_quandl.py
@@ -3,6 +3,8 @@ import pytest
 from io import StringIO
 from unittest.mock import MagicMock, patch
 from data_ingestion import fetch_quandl_data
+import os
+from dotenv import load_dotenv
 
 
 @pytest.fixture
@@ -17,33 +19,24 @@ def mock_read_csv(monkeypatch: pytest.MonkeyPatch) -> Generator[MagicMock, None,
         yield mock_read_csv
 
 
-@pytest.fixture
-def mock_quandl_api_key(monkeypatch: pytest.MonkeyPatch) -> None:
-    monkeypatch.setattr("data_ingestion.fetch_quandl.QUANDL_API_KEY", "mock_api_key")
-
-
-def test_fetch_quandl_data(
-    mock_read_csv: MagicMock, mock_get: MagicMock, mock_quandl_api_key: None
-) -> None:
-    # Mock the response from requests.get
+def test_fetch_quandl_data(mock_read_csv: MagicMock, mock_get: MagicMock) -> None:
+    load_dotenv(verbose=False)
+    QUANDL_API_KEY = os.getenv("QUANDL_API_KEY", "mock_api_key")
+    
     mock_response = mock_get.return_value
     mock_response.text = "Date,Open,High,Low,Close,Volume,Ex-Dividend,Split Ratio,Adj. Open,Adj. High,Adj. Low,Adj. Close,Adj. Volume\n2020-01-01,100.0,105.0,95.0,102.0,1000000,0.0,1.0,100.0,105.0,95.0,102.0,1000000\n"
     mock_csv_data = StringIO(mock_response.text)
     mock_response.status_code = 200
-
-    # Mock the return value of pd.read_csv
     mock_data = mock_read_csv.return_value
 
-    # Call the function under test
     data = fetch_quandl_data("AAPL", start_date="2020-01-01", end_date="2020-01-02")
 
-    # Assert that the function behaves as expected
     mock_get.assert_called_once_with(
         "https://www.quandl.com/api/v3/datasets/WIKI/AAPL.csv",
         params={
             "start_date": "2020-01-01",
             "end_date": "2020-01-02",
-            "api_key": "mock_api_key",
+            "api_key": QUANDL_API_KEY,
         },
     )
     actual_call_arg = mock_read_csv.call_args[0][0]
