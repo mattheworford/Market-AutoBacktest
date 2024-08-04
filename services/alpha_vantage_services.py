30ce118fd48c2c79f318af65a55349f4cf46986d
WCS19 <wcs@bu.edu>
2024-08-04 14:14:53 -0400

    created av data service class

---
+++ services/alpha_vantage_services.py
@@ -0,0 +1,14 @@
+# services/data_service.py
+from typing import Callable, Type
+from dataclasses_json import DataClassJsonMixin
+from client.alpha_vantage import AlphaVantageApiClient
+
+class DataService:
+    def __init__(self, api_client: AlphaVantageApiClient):
+        self.api_client = api_client
+
+    def fetch_and_transform_data(self, endpoint_constructor: Callable[..., str], model: Type[DataClassJsonMixin], **params) -> DataClassJsonMixin:
+        return self.api_client.get_data(endpoint_constructor, model, **params)
+
+    def get_json_response(self, data: DataClassJsonMixin) -> str:
+        return data.to_json(indent=4)
