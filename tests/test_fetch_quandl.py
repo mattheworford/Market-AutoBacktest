11948fa233b2a6ad4dab3f3f7c866f0d40c6ee5d
Matthew Orford <mattorford9@gmail.com>
2024-08-05 00:52:25 -0400

    refactor to use env

--- tests/test_fetch_quandl.py
+++ tests/test_fetch_quandl.py
@@ -4,6 +4,8 @@ from io import StringIO
 from unittest.mock import MagicMock, patch
 from config.config import QUANDL_API_KEY
 from data_ingestion import fetch_quandl_data
+import os
+from dotenv import load_dotenv
 
 
 @pytest.fixture
@@ -19,19 +21,17 @@ def mock_read_csv(monkeypatch: pytest.MonkeyPatch) -> Generator[MagicMock, None,
 
 
 def test_fetch_quandl_data(mock_read_csv: MagicMock, mock_get: MagicMock) -> None:
-    # Mock the response from requests.get
+    load_dotenv(verbose=False)
+    QUANDL_API_KEY = os.getenv("QUANDL_API_KEY")
+    
     mock_response = mock_get.return_value
     mock_response.text = "Date,Open,High,Low,Close,Volume,Ex-Dividend,Split Ratio,Adj. Open,Adj. High,Adj. Low,Adj. Close,Adj. Volume\n2020-01-01,100.0,105.0,95.0,102.0,1000000,0.0,1.0,100.0,105.0,95.0,102.0,1000000\n"
     mock_csv_data = StringIO(mock_response.text)
     mock_response.status_code = 200
-
-    # Mock the return value of pd.read_csv
     mock_data = mock_read_csv.return_value
 
-    # Call the function under test
     data = fetch_quandl_data("AAPL", start_date="2020-01-01", end_date="2020-01-02")
 
-    # Assert that the function behaves as expected
     mock_get.assert_called_once_with(
         "https://www.quandl.com/api/v3/datasets/WIKI/AAPL.csv",
         params={
